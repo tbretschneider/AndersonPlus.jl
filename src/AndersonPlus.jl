1cd151a40ff1f62df3fa7302648c4f5aba68da2b
Tobias Bretschneider <tobias@ehgraphicdesign.co.uk>
2025-01-18 16:03:01 +0000

    exporting issues

--- src/AndersonPlus.jl
+++ src/AndersonPlus.jl
@@ -2,14 +2,12 @@ module AndersonPlus
 
 import LinearAlgebra
 
-export greet_your_package_name
+export greet_your_package_name, p1_f!, AASolve
 include("0_Structs.jl")
 include("2_HelperFunctions.jl")
 include("3_AndersonFunctions.jl")
 include("4_AnalysisFunctions.jl")
-export p1_f!
 include("5_ProblemsSimple.jl")
-export AASolve
 include("1_Functions.jl")
 
 end
