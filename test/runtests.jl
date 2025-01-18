f94428d0326c6c592b056cda8b20a63ba0cff39a
Tobias Bretschneider <tobias@ehgraphicdesign.co.uk>
2025-01-18 17:57:09 +0000

    WillDefoFail

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,8 @@ using Test
 
 include("runhelpertests.jl")
 
+include("runmethodtests.jl")
+
 @testset "AndersonPlus.jl" begin
     @test AndersonPlus.greet_your_package_name() == "Hello YourPackageName!"
     @test AndersonPlus.greet_your_package_name() != "Hello world!"
