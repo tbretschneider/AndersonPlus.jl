1343a1c80d607939fdf73fac8da574c3ce3dd2ff
Tobias Bretschneider <tobias@ehgraphicdesign.co.uk>
2025-01-18 17:59:08 +0000

    exporting properly

--- src/0_Structs.jl
+++ src/0_Structs.jl
@@ -1,26 +1,31 @@
 # Structure for the output of the algorithm
 
+export AAMethod
 struct AAMethod
     methodname::Symbol
 	methodparams::NamedTuple
 end
 
+export AAAlgorithm
 struct AAAlgorithm
     method::AAMethod
     algorithmparams::NamedTuple
 end
 
+export AAConvParams
 struct AAConvParams
 	rtol::Float64
     atol::Float64
 end
 
+export AAProblem
 struct AAProblem
     GFix!::Function                 # Fixed-point map (GFix!(G, x))
     x0::Vector{Float64}			# Initial iterate
     convparams::AAConvParams
 end
 
+export AAAnalysis
 struct AAAnalysis
     liveanalysis::Vector{Symbol}
     midanalysis::Vector{Symbol} # Metrics to track (e.g., :residual, :iterations)
@@ -29,16 +34,19 @@ struct AAAnalysis
     summary::Bool
 end
 
+export AAInput
 struct AAInput
     problem::AAProblem               # Fixed-point map (GFix!(G, x))
     algorithm::AAAlgorithm			# Initial iterate
     analyses::AAAnalysis
 end
 
+export AAAnalysisOutput
 struct AAAnalysisOutput
     output::NamedTuple # Metrics to track (e.g., :residual, :iterations)
 end
 
+export AAOutput
 struct AAOutput
     solution::Vector{Float64}        # Converged result
     input::AAInput   # G(solution)
