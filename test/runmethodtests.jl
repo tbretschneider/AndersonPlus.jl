77337e3c25f514744a281ba3e9cae17416a8ebd1
Tobias Bretschneider <tobias@ehgraphicdesign.co.uk>
2025-01-19 21:00:18 +0000

    test should work...

--- test/runmethodtests.jl
+++ test/runmethodtests.jl
@@ -15,12 +15,12 @@ using AndersonPlus
                             (maxit = 20, ))
 
     Analyses = AAAnalysis([],
-                        [:residualnorm,:G_k_cond,:alpha_k_norm],
+                        [:residualnorm,:G_k_cond,:alpha_k_norm_l1],
                         0,false)
     
     Output = AASolve(AAInput(Problem,Algorithm,Analyses))
 
     @test isapprox(Output.analysis.output.residualnorm,residual_norm,rtol=5e-3)
     @test isapprox(Output.analysis.output.G_k_cond[1:7],condition_number[1:7],rtol=5e-3)
-    @test isapprox(Output.analysis.output.alpha_k_norm,coefficient_norm,rtol=5e-3)
+    @test isapprox(Output.analysis.output.alpha_k_norm_l1,coefficient_norm,rtol=5e-3)
 end
