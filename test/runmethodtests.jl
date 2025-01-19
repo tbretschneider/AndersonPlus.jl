022beb261b416c1a30692b4490ecda73c1e1b558
Tobias Bretschneider <tobias@ehgraphicdesign.co.uk>
2025-01-19 21:09:52 +0000

    Stuff...

--- test/runmethodtests.jl
+++ test/runmethodtests.jl
@@ -22,5 +22,5 @@ using AndersonPlus
 
     @test isapprox(Output.analysis.output.residualnorm,residual_norm,rtol=5e-3)
     @test isapprox(Output.analysis.output.G_k_cond[1:7],condition_number[1:7],rtol=5e-3)
-    @test isapprox(Output.analysis.output.alpha_k_norm_l1[2:7],coefficient_norm[2:7],atol=5e-2)
+    @test isapprox(Output.analysis.output.alpha_k_norm_l1[2:7],coefficient_norm[2:7],atol=5e-3)
 end
