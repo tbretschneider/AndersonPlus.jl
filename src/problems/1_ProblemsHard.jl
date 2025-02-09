a13de5acf39b1c328fa3c99fcd0a6700024bdba5
Tobias Bretschneider <tobias@ehgraphicdesign.co.uk>
2025-02-09 23:16:08 +0000

    fixedp3?

--- src/problems/1_ProblemsHard.jl
+++ src/problems/1_ProblemsHard.jl
@@ -234,11 +234,11 @@ x_end = 10.0
 x = range(x_start, x_end, length=N+1)
 
 function p3_f!(G,u)
-    G .= AndersonPlus.p3_f_helper!(u, k0, ε, N)
+    G .= AndersonPlus.p3_f_helper!(copy(u), k0, ε, N)
 end
 
 function p3_f!(G,u,x0,ε, N)
-    G .= AndersonPlus.p3_f_helper!(u, k0, ε, N)
+    G .= AndersonPlus.p3_f_helper!(copy(u), k0, ε, N)
 end
 
 u_re = cos.(k0 * x)
